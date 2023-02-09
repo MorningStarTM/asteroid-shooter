d5f2d0ce48c1650f20f3040e13af3b0ab59b5d2a
ErnestBeckham <ernestbeckham1998@gmail.com>
2023-02-09 18:30:58 +0530

    game updated

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import time
 from fire import Nuclear, Missile
 from asteroid import Asteroid
+from animation import Explosion
 
 pygame.init()
 
@@ -153,45 +154,6 @@ class Bullet(pygame.sprite.Sprite):
             explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
             Explosion_group.add(explosion)
 
-
-#class for explosion
-class Explosion(pygame.sprite.Sprite):
-    def __init__(self, x, y, size):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
-        for eps in range(1,6):
-            #load explosion image
-            img = pygame.image.load(f'./image/exp{eps}.png')
-            #explosion size
-            if size == 1:
-                img = pygame.transform.scale(img, (20, 20))
-            elif size == 2:
-                img = pygame.transform.scale(img, (40, 40))
-            elif size == 3:
-                img = pygame.transform.scale(img, (160, 160))
-            elif size == None:
-                img = pygame.transform.scale(img, (400, 400))
-            #append the img into list
-            self.images.append(img)
-        self.index = 0
-        self.image = self.images[self.index]
-        self.rect = self.image.get_rect()
-        self.rect.center = [x,y]
-        self.counter = 0
-
-    
-    def update(self):
-        explosion_speed = 3
-        self.counter += 1
-
-        if self.counter >= explosion_speed and self.index < len(self.images) - 1:
-            self.counter = 0
-            self.index += 1
-            self.image = self.images[self.index]
-
-        if self.index >= len(self.images) - 1 and self.counter >= explosion_speed:
-            self.kill()
-        
   
 
 
@@ -200,7 +162,7 @@ Spaceship_group = pygame.sprite.Group()
 Bullet_group = pygame.sprite.Group()
 Missile_group = Missile.sprite_groups()
 Nuclear_group = Nuclear.sprite_groups()
-Explosion_group = pygame.sprite.Group()
+Explosion_group = Explosion.sprite_groups()
 
 all_sprite = pygame.sprite.Group()
 asteroid_group = Asteroid.sprite_groups()
